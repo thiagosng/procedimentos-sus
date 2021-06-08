0bdbd150d1161a078b4fef82d537cf63f086fc54
thiagosng <thiagonunes026@gmail.com>
2021-06-08 16:56:38 -0300

    Formulario Unform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import './App.css';
+import Input from './components/Form/input';
 
 function App() {
+  const formRef = useRef(null);
+
+  const initialData = {
+    name: 'Thiago'
+  }
+  function handleSubmit(data, { reset }){
+    if(data.name === ""){
+      formRef.current.setFieldError('name', 'O nome é obrigatório');
+    }
+    console.log(data);
+
+    reset();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ABCDE</h1>
+
+      <Form ref={formRef} initialData={initialData} onSubmit={handleSubmit}>
+        
+          <Input type="text" name="group" />
+        
+        
+          <Input name="sub-group" />
+        
+        
+          <Input name="organization-form" />
+        
+        
+          <Input name="code" />
+        
+        
+          <Input name="name" />
+        
+
+        <button type="submit">Enviar</button>
+      </Form>
+
     </div>
   );
 }
